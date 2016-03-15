125552d7e909856a420cb8742a80e1677fb6aef6
robertmarchese <robertmarchese01@gmail.com>
2016-03-15 09:30:22 -0400

    aaa
    faffds

--- js/getnav.js
+++ js/getnav.js
@@ -1,4 +1,4 @@
-$.get("http//:robertmarchese.github.io/partials/nav.html", function (data) {
+$.get("http://robertmarchese.github.io/partials/nav.html", function (data) {
 
 	$(document).ready(function () {
 
