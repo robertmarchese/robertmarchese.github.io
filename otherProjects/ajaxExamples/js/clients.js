3665400f2b4c74a2e8943876dee7c0f5b2dc14d7
robertmarchese <robertmarchese01@gmail.com>
2016-03-15 12:41:19 -0400

    aaaa

--- otherProjects/ajaxExamples/js/clients.js
+++ otherProjects/ajaxExamples/js/clients.js
@@ -1,19 +1,26 @@
 $(document).ready(function () {
 
 		$("#getClients").on("click", function () {
-
 				var url = "http://robertmarchese.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json";
 
 				$.getJSON("http://robertmarchese.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json", function (data) {
+						var html = "<table class='table table-hover table-striped'>" +
+							"<tr><th>Name</th><th>Email</th><th>Company</th></tr>";
 
 						$.each(data, function (index, item) {
-								$("#data").append(item.name);
-							})
-							//alert(data);
-							//console.dir(data);
+
+							html += "<tr>" +
+								"<td>" + item.name + "</td>" +
+								"<td>" + item.email + "</td>" +
+								"<td>" + item.company + "</td>" +
+								"</tr>";
+						})
+
+						html += "</table>";
+						$("#data").append(html);
+						//alert(data);
+						//console.dir(data);
 
 					}) //getJSON
-
 			}) //click
-
 	}) //ready
