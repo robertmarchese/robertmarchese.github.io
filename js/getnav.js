37c397633123726cd17dc1638fa634ab32f9d8f8
robertmarchese <robertmarchese01@gmail.com>
2016-03-15 09:12:39 -0400

    aa

--- js/getnav.js
+++ js/getnav.js
@@ -1,16 +1,11 @@
-$.get("http://robertmarchese.github.io/partials/nav.html", function (data) {
+$.get("http//:robertmarchese.github.io/Partial/nav.html", function (data) {
 
 	$(document).ready(function () {
 
 		//check if this document has .container
-		var container = $(".container");
+		$(".container").prepend(data);
 
-		//if so add nav.html contents to the top
-		if (container) {
-			container.prepend(data)
-			container.fadeIn();
-		}
 
-	});
+	})
 
-});
+})
