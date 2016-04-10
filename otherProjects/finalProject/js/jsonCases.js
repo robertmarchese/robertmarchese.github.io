1f9ba78c6caa1802fde586ec1fa15e80d33cdda5
robertmarchese <robertmarchese01@gmail.com>
2016-04-10 11:44:38 -0400

    Final Project added

---
+++ otherProjects/finalProject/js/jsonCases.js
@@ -0,0 +1,65 @@
+$(document).ready(function () {
+
+	$.getJSON("jsonDatabase/jsonCases.json", function (data) {
+
+		console.dir(data);
+		var html = "";
+
+		$.each(data, function (index, item) {
+				html += '<div class="col-md-4">' +
+					'<div class="caseType">' + item.type + '</div>' +
+					'<div class="caseName">' + item.name + '</div>' +
+					'<div class="caseColour">' + item.colour + '</div>' +
+					'<img class="Casesimage" src="' + item.image + '"/>' +
+					'<div class= "commentsContainer">';
+
+				$.each(item.comments, function (ind, i) {
+
+						html += '<div class="buyerName">' + i.username + '</div>' +
+							'<div class="buyerComment">' + i.comment + '</div>' +
+							'<div class="buyerStars">';
+
+						var numStars = Number(i.stars);
+						for (var i = 1; i <= 5; i++) {
+							if (i <= numStars) {
+								html += '<img src="http://robertmarchese.github.io/otherProjects/ajaxExamples/images/star.png">';
+							} else {
+								html += '<img src="http://robertmarchese.github.io/otherProjects/ajaxExamples/images/nostar.png">';
+							}
+						}
+
+						html += '</div>'; // stars div
+
+
+					}) // each commment
+
+				html +=
+
+					'</div>' + //commentcontainer
+					'</div>'; // col md div
+			}) //eachcat
+
+		$("#caseData").append(html);
+
+	})
+
+})
+
+
+/*
+
+<div class="col-md-4 cases">
+<div class="caseName"> </div>
+<div class="caseType"> </div>
+<div class="caseColour"> </div>
+<img src=""/>
+<div class="commentsContainer">  // one per comment
+<div class="buyerName"> </div>
+<div class="buyerComment"> </div>
+<div class="buyerStars"> </div>
+</div>
+
+
+</div>
+
+*/
---
+++ otherProjects/finalProject/js/takeAnOrder.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+		var log = $("#log");
+		//click event
+		//user wants to order
+		$("button").on("click", function () {
+
+			log.append($("input").val() + "<br/>")
+				.append($("textarea").val() + "<br/>")
+				.append($("select").val() + "<br/>")
+				.append($("[name='gender']:checked").val() + "<br/>")
+				.append($('input:checkbox:checked').map(function () {
+					return this.value;
+				}).get().join());
+
+		})
+
+		//focus event
+		//chnages background to grey
+		$("input[type='text']").on("focus", function () {
+			log.append("User focussed on the input <br/>");
+			$(this).css("background-color", "grey");
+
+		})
+
+		//blur event
+		//chnages background to whilte
+		$("input[type='text']").on("blur", function () {
+			log.append("User moved away from the input <br/>");
+			$(this).css("background-color", "white");
+
+		})
+
+		//mouseenter mouseleave envent
+		//changes message on the button text
+		$("button").on("mouseenter", function () {
+				log.append("User is hovering on button <br/>");
+				$(this).text("Order Now");
+
+			})
+			.on("mouseleave", function () {
+				log.append("User is no longer hovering on the button <br/>");
+				$(this).text("Order Now");
+
+			})
+
+		//select event
+		//writes some html on the screen depending on the selection
+		$("select").on("change", function () {
+			log.append("User changed selection <br/>");
+			var selInfo = $("#selectInfo"),
+				choice = $(this).val();
+
+			if (choice == "Galaxy S6") {
+				selInfo.html("Galaxy S6");
+			} else if (choice == "Iphone 6S") {
+				selInfo.html("Iphone 6S");
+			}
+		})
+
+	}) //end doc ready
